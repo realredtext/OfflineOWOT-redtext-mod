ac727dfd5f653c7a4c928ab1a35749f53122c801
redtext <100484201+realredtext@users.noreply.github.com>
2022-07-25 20:47:50 -0400

    Create state.js

---
+++ static/js/state.js
@@ -0,0 +1,26 @@
+//this is the permission shit, feel free to mod
+var state = {
+    "userModel": {
+        "username":"fp",
+        "is_superuser":true,
+        "authenticated":true,
+        "is_member":true,
+        "is_owner":true,
+        "is_staff":true,
+        "is_operator":true
+    },
+    "worldModel": { 
+        "feature_membertiles_addremove":true,
+        "writability":0,
+        "feature_url_link":0,
+        "feature_go_to_coord":0,
+        "name":"",
+        "feature_paste":0,
+        "namespace":"",
+        "readability":0,
+        "feature_coord_link":0,
+        "pathname":"",
+        "chat_permission":0,
+        "color_text":0
+    }
+};
